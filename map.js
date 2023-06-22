1eb43c6bc9997f3e2b70bb8e1151c4f2d4313431
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2023-06-22 15:06:39 -0300

    generateCoordinatesFunction

--- map.js
+++ map.js
@@ -7,3 +7,37 @@ function initMap() {
     var map = new
     google.maps.Map(document.getElementById('map'), options);
 }
+
+const btnGenerateRoutes = document.querySelector("#btnGenerateRoutes");
+
+btnGenerateRoutes.addEventListener("click",function(e) {
+    e.preventDefault();
+
+    const ipDepartureAdress = document.querySelector("#ipDepartureAdress");
+    var departureAdress = ipDepartureAdress.value;
+
+    generateCoordinates(departureAdress);
+
+    console.log(departureAdress);
+});
+
+
+function generateCoordinates(inputAddress) {
+  const address = inputAddress;
+  const apiKey = "AIzaSyCMekkkZ9oy-zoKC5WUQq6ws2d6pQV9Yk4";
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    if (data.status === 'OK') {
+      const latitude = data.results[0].geometry.location.lat;
+      const longitude = data.results[0].geometry.location.lng;
+      console.log(`Latitude: ${latitude}`);
+      console.log(`Longitude: ${longitude}`);
+    }
+  })
+  .catch(error => {
+    console.error("Ocorreu um erro ao obter as coordenadas:", error);
+  });
+}
